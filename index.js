f698785af5bd72b3a72a9e70e4e01e294200326d
SandaruwanB <sandarusbandara110@gmail.com>
2026-03-28 07:11:32 +0530

    [DEV] establish mysql db connection

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const http = require('http');
+const cors = require('cors');
 const { WebSocketServer } = require('ws');
 const path = require('path');
 const dotnev = require('dotenv');
+
 
 dotnev.config({path: path.join(__dirname, '.env')});
 
@@ -14,7 +16,9 @@ const port = process.env.PORT || 8080;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '10mb'}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/static', express.static(path.join(__dirname, 'views/static')));
 
@@ -31,7 +35,6 @@ wss.on('connection', (ws) => {
     console.log('Client connected');
 
     ws.send(JSON.stringify({ type: 'info', message: 'Connected to WebSocket server' }));
-
     ws.on('message', (raw) => {
         try {
             const data = JSON.parse(raw);
@@ -41,12 +44,10 @@ wss.on('connection', (ws) => {
             ws.send(JSON.stringify({ type: 'error', message: 'Invalid JSON' }));
         }
     });
-
     ws.on('close', () => console.log('Client disconnected'));
 });
 
 app.get('/', (req, res) => res.redirect('/web/login'));
-
 app.use('/api', require('./routes/api')(broadcast));
 app.use('/web', require('./routes/web')(broadcast));
 
